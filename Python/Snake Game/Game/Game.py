8a26fd3806d808bc557752a16443afa8867b8978
General Kenobi <augut.hummert5@gmail.com>
2019-02-25 11:01:56 -0600

    2/25
    Programming Day 1, setup pycharm and first tut

---
+++ Python/Snake Game/Game/Game.py
@@ -0,0 +1,30 @@
+import pygame
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((300,300))
+    pygame.display.set_caption('Basic Pygame Program')
+
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((250, 250, 250))
+
+    font = pygame.font.Font(None, 36)
+    text = font.render("Hello There", 1, (10, 10, 10))
+    textpos = text.get_rect()
+    textpos.centerx = background.get_rect().centerx
+    textpos.centery = background.get_rect().centery
+    background.blit(text, textpos)
+
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
+
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.blit(background, (0,0))
+        pygame.display.flip()
+
+if __name__ == '__main__': main()
